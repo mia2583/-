65fc9b81f148ff4be3c43146fac55d294582cd2d
hyun284 <126371387+hyun284@users.noreply.github.com>
2023-03-10 16:54:00 +0900

    [BOJ] 또 전자레인지야?? / 실버 1 / 2시간 이상

--- 박현수/[BOJ] 24390.py
+++ 박현수/[BOJ] 24390.py
@@ -1,32 +1,33 @@
 from collections import deque
-T = int(input())
-for i in range(T):
-    H, W, O, F, Xs, Ys, Xe, Ye = map(int, input().split(" "))
-    graph = [[0]*W for _ in range(H)]
-    check = [[0]*W for _ in range(H)]
-    force = [[-1]*W for _ in range(H)]
-    for q in range(O):
-        i, j, k = map(int, input().split(" "))
-        graph[i-1][j-1] = k
-    start, end = (Xs-1, Ys-1), (Xe-1, Ye-1)
-    force[start[0]][start[1]] = F
-    queue = deque([start])
-    dx = [1,-1,0,0]
-    dy = [0,0,-1,1]
-
-    while queue:
-        x,y = queue.popleft()
-        for i in range(4):
-            new_x = x + dx[i]
-            new_y = y + dy[i]
-
-            if(new_x>=0 and new_x < H and new_y >= 0 and new_y < W):
-                if(check[new_x][new_y] == 0):
-                    if(force[x][y] >= graph[new_x][new_y]-graph[x][y]):
-                        check[new_x][new_y] = 1
-                        queue.append((new_x,new_y))
-                        force[new_x][new_y] = force[x][y]-1
-    if force[end[0]][end[1]] >= 0:
-        print("잘했어!!")
-    else:
-        print("인성 문제있어??")
+visited = []
+min, sec = map(int, input().split(":"))
+a,b,c,s = 10, 60, 600, 30
+graph = {s:[c,b,a], a:[a], b:[b,a], c:[c,b,a]}
+total = min*60 + sec
+if sec >= 30:
+    root = s
+elif total >= 600:
+    root = c
+elif total >= 60:
+    root = b
+else:
+    root = a
+count = 0
+queue = deque([[root, total-root, 1]])
+while queue:
+    n = queue.popleft()
+    if n[1] == 0 and n[2] == 1:
+        count += 1
+        break
+    for i in graph[n[0]]:
+        if n[1]-i < 0:
+            continue
+        queue.append([i, n[1]-i,n[2]+1])
+        if (n[1]-i) == 0:
+            count = n[2]+1
+            break
+        break
+if root == s:
+    print(count)
+else:
+    print(count + 1)
